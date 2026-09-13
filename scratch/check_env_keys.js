646dd3ed2d657e1bbec4d7d6d736d1e124778c76
fraylontechnologies-boop <fraylontechnologies@gmail.com>
2026-09-13 14:53:47 +0530

    gggcdncddc

--- scratch/check_env_keys.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const env = fs.readFileSync('.env.local', 'utf8');
-const lines = env.split('\n').map(l => l.trim()).filter(l => l && !l.startsWith('#'));
-console.log('Available env keys:');
-lines.forEach(l => {
-  const k = l.split('=')[0];
-  console.log(' - ' + k);
-});
